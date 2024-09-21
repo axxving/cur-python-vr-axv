c0883ef0328f855ddcfbffbfcd224e7974e7102e
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-09-21 15:55:21 -0600

    feat: exercise 01
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

---
+++ 01-Ejercicios/01-ejercicio.py
@@ -0,0 +1,13 @@
+"""
+    Ejercicio - 01
+    
+        - Hacer dos variables, una "pais" y otra "continente"
+        - Mostrar su valor en la pantalla 
+        - Poner un comentario diciento el tipo de dato
+"""
+
+pais = 'Mexico'
+continente =  'America'
+
+print(f"El valor de pais es: {pais}, y el valor de continente es: {continente}")
+print(f"El tipo de dato de ambas variables es {type(continente)}")
