7d8276eb1d7d866dbb9bf82c50c4a92e4df65b38
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-09-21 16:04:44 -0600

    feat: exercise 02
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

---
+++ 01-Ejercicios/02-ejercicio.py
@@ -0,0 +1,9 @@
+"""
+    Ejercicio 02
+    
+        - Escribir un script que nos muestre por pantalla todos los numeros pares del 1 al 100
+"""
+
+for par in range(1, 100):
+    if (par % 2 == 0):
+        print(par)
