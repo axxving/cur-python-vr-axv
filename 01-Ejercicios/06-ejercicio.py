25f4ab9d4f991fb2e0b5c97269d72d2c686ad495
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-09-21 18:36:30 -0600

    feat: exercise 06
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

---
+++ 01-Ejercicios/06-ejercicio.py
@@ -0,0 +1,11 @@
+"""
+    Ejercicio 02
+    
+        - Mostrar todas las tablas de multiplicar
+"""
+
+for multiplicado in range(1,11):
+    multiplicado
+    for  multiplicador in range(1,11):
+        print(f"{multiplicador} x {multiplicado} = {multiplicador * multiplicado}")
+        
