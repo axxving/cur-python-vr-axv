8e192597ffa180a687f384844c12f13a0c0a0937
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-09-21 19:54:45 -0600

    feat: exercise 09
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

---
+++ 01-Ejercicios/09-ejercicio.py
@@ -0,0 +1,9 @@
+"""
+    Ejercicio 07
+    
+        - Pide un numero indefinidamente hasta que el usuario ingrese 111
+"""
+numero = 0
+
+while (numero != 111):
+    numero = int(input('Ingresa un numero: '))
