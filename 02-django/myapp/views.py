6bc7e38fbc9423dbde67af3ba2ebc8efcffeeb87
axxving <axxving@gmail.com>
2024-10-14 16:26:10 -0600

    feat: views and URLs
    Co-Authored-By: axxving <160192346+axxving@users.noreply.github.com>

--- 02-django/myapp/views.py
+++ 02-django/myapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+#  ?
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Inicio.")
+
+def mi_vista(request):
+    return HttpResponse("Hola, esta es mi primera vista en django.")
+
+# Views probadas en backend
+@csrf_exempt
+def mi_vista_api(request):
+    if request.method == 'GET':
+        return JsonResponse({"message": "Esta es una solicitud GET"})
+
+    elif request.method == 'POST':
+        data = request.POST.get('data', 'No se envió data')
+        return JsonResponse({"message": "Esta es una solicitud POST", "data": data})
+
+    else:
+        return JsonResponse({"error": "Método no permitido"}, status=405)
