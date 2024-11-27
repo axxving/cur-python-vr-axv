d024cea12d5e24546735da9980f6fe48c10463b4
axxving <axxving@gmail.com>
2024-11-27 13:39:50 -0600

    feat: hello world

---
+++ 01-holamundo/holamundo.py
@@ -0,0 +1,5 @@
+# Para hacer una impresion en consola, se utiliza el print().
+# - Print, imprime en consola
+# - Las comillas, sirve para ingresar una cadena de texto
+# - Print es una funcion
+print('Hello World With Python.')
